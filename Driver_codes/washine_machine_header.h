d84c31e29fb0d2ca036cd959d49a38ce87da4560
Saumya <134420469+saumyaverma991@users.noreply.github.com>
2025-06-30 00:25:29 +0530

    Create washine_machine_header.h

---
+++ Driver_codes/washine_machine_header.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   washing_machine_function_def.h
+ * Author: Saumya
+ *
+ * Created on 12 June, 2025, 7:30 PM
+ */
+
+#ifndef WASHING_MACHINE_HEADER_H
+#define WASHING_MACHINE_HEADER_H
+
+void power_on_screen(void);
+void clear_screen(void);
+void washing_program_display( unsigned char key);
+void water_level_screen( unsigned char key);
+void set_time(void);
+void run_program(unsigned  char key);
+void door_status_check(void);
+
+
+#endif /* WASHING_MACHINE_HEADER_H */
